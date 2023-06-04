49b0cd203de94710bdf09d1e4196f057b095e073
sakshi0325 <sakshipahariya03@gmail.com>
2023-06-04 22:50:40 +0530

    Initial commit

--- pascal.cpp
+++ pascal.cpp
@@ -1,20 +1,16 @@
-#include<bits/stdc++.h>
-class Solution {
-public:
-    vector<vector<int>> generate(int numRows) {
-    int i = 0, j;
-    vector<vector<int>> pascal(numRows); 
-       while(i<numRows)
-       {
-           j=0;
-           while(j<=i)
-           {
-               if(j==0 || j==i) pascal[i].push_back(1);
-               else pascal[i].push_back(pascal[i-1][j-1]+pascal[i-1][j]);
-               j++;
-           }
-           i++;
-       }
-       return pascal; 
+#include <bits/stdc++.h>
+
+
+vector<vector<long long int>> printPascal(int n) 
+{
+  // Write your code here.
+  vector<vector<long long int>>res;
+  for(int i=0;i<n;i++){
+    vector<long long int>v(i+1,1);
+    for(int j=1;j<i;j++){
+      v[j]=res[i-1][j-1]+res[i-1][j];
     }
-};
+    res.push_back(v);
+  }
+  return res;
+}
