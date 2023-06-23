b055334978b4d6afb3c9c8db0fd9b202a04b4dd0
sakshi0325 <sakshipahariya03@gmail.com>
2023-06-23 23:18:23 +0530

    Added

---
+++ Binary Tree/Inorder.cpp
@@ -0,0 +1,8 @@
+vector<int> v;
+    vector<int> inorderTraversal(TreeNode* root) {
+     if(root==NULL) return v;
+     inorderTraversal(root->left);
+     v.push_back(root->val);
+     inorderTraversal(root->right);
+    return v;
+    }
---
+++ Binary Tree/Preorder.cpp
@@ -0,0 +1,8 @@
+vector<int> v;
+    vector<int> preorderTraversal(TreeNode* root) {
+     if(root==NULL) return v;
+     v.push_back(root->val);
+     preorderTraversal(root->left);
+     preorderTraversal(root->right);
+     return v;
+    }
