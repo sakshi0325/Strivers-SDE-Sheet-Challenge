a4ce4bc165fefe351cc8fe60228a114b47625c85
sakshi0325 <sakshipahariya03@gmail.com>
2023-07-11 23:47:04 +0530

    Added

---
+++ Binary Tree II/diameterOfBT.cpp
@@ -0,0 +1,16 @@
+public:
+int maxdepth=0;
+    int height(int maxDepth, TreeNode* root)
+    {
+        if(root==NULL)return 0;
+        int lh=height(maxdepth,root->left);
+        int rh=height(maxdepth,root->right);
+        maxdepth=max(maxdepth,lh+rh);
+        return 1+max(lh,rh);
+        
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans=height(maxdepth,root);
+        return maxdepth;
+       
+    }
